4076041b4437422aafe6a88baf02f89e9e6c6d25
TalissonUni <202501477641@alunos.UNIFAVIP.edu.br>
2025-12-04 00:08:37 +0000

    aula4

---
+++ emprestimolivro.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Configuracao de sucesso! Pressione ENTER para sair.\n");
+    // Adiciona uma pausa para que o terminal nao feche imediatamente
+    getchar(); 
+    return 0;
+}
